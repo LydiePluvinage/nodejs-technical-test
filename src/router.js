cf6bdb89a72ca21e07f8255bb066ad5c25a8eda6
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-03-20 17:21:44 +0100

    Users GET branch is operational

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const usersController = require('./controllers/users');
+const authController = require('./helpers/auth');
 
 const setupRoutes = (server) => {
   // subscribe route goes to users controller. We will register new users here
-  server.use('/subscribe', usersController.emailIsFree, usersController.subscribe);
-  server.use('/login', usersController.emailExists, usersController.login);
+  server.post('/subscribe', usersController.emailIsFree, usersController.subscribe);
+  server.post('/login', usersController.emailExists, usersController.login);
+  server.get('/users', authController.checkAuthorization, usersController.findAll);
 };
 
 module.exports = setupRoutes;
